ffc8c57724aa3586b7971f180ccd487ff16ff60e
Noff22 <Noff22@users.noreply.github.com>
2015-12-29 20:21:36 -0500

    Added Menu

--- js/gameScript.js
+++ js/gameScript.js
@@ -11,18 +11,21 @@ $(document).ready(function(){
     setBlock("#b2");
     setBlock("#b3");
     setBlock("#b4");
+	//$(".option").each(animateDiv);
     console.log("MarginX " + marginX);
     console.log("MarginY " + marginY);
 });
 
-function setBlock(tile) {
+function setBlock(block) {
     var x = genX();
     var y = genY();
 	
-    $(tile).css({
+    $(block).css({
         "left": x,
         "top": y       
      });
+	 
+	
 }
 
 function genX() {
@@ -34,3 +37,45 @@ function genY() {
     var y = Math.floor(Math.random() * (window.innerHeight-marginY*2))+marginY;
 	return y;
 }
+
+//http://codepen.io/anon/pen/myyzXV
+/*
+function animateDiv(){
+    var newq = makeNewPosition();
+    var oldq = $('.option').offset();
+    var speed = calcSpeed([oldq.top, oldq.left], newq);
+	
+    $('.option').animate({ top: newq[0], left: newq[1] }, speed, function(){
+      animateDiv();        
+    });
+    
+};
+
+function makeNewPosition(){
+    
+    // Get viewport dimensions (remove the dimension of the div)
+    var h = $(window).height() - 50;
+    var w = $(window).width() - 50;
+    
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+    
+    return [nh,nw];    
+    
+}
+
+function calcSpeed(prev, next) {
+    
+    var x = Math.abs(prev[1] - next[1]);
+    var y = Math.abs(prev[0] - next[0]);
+    
+    var greatest = x > y ? x : y;
+    
+    var speedModifier = 0.05;
+
+    var speed = Math.ceil(greatest/speedModifier);
+
+    return speed;
+
+}
+*/
