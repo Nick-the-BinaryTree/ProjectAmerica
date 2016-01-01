9e25b006129e20db1cd056ec53e88549e9c1e411
komal15 <komaldhull@yahoo.com>
2016-01-01 10:38:42 -0500

    correct answers work, false in progress

--- js/gameScript.js
+++ js/gameScript.js
@@ -251,6 +251,217 @@ function getQuestions(ranCategory){
     }
 
 }
+//Correct is the current event whose information is correct for all questions
+//Types are ints that specify what kind of false answers to generate
+//1 is year, 2 is location, 3 is battle notable, 4 is invention significance, 5 is election notable, 6 is court ruling, 7 is other significance
+
+function genFalseAnswers(questionType){
+    var usedAnswers=[];
+    var numFalse = 3;
+    var timeout = 0;
+    var timeoutFallback = 1000;
+    var timeoutMax = 1500;
+    
+    if(questionType === 1){ //Year (Works for any type)
+        
+        usedAnswers.push(correct.year);
+            
+        while(usedAnswers.length < numFalse){
+            
+            var potential = getRanYear();
+            
+            if(notUsed(potential, usedAnswers)){
+                usedAnswers.push(potential);
+            }
+            
+            timeout++;
+            
+            if(timeout>timeoutMax){ //If nothing was found in a large number of searches, hide the bubble
+                usedAnswers.push(-1);  //Bubble hiding code
+                break;
+            }
+        }
+        
+        var falseAnswers = usedAnswers.splice(1,usedAnswers.length); //Removes first element (correct answer)
+        
+        return falseAnswers;
+    }
+    else if(questionType === 2){ //Location (Only called for battles)
+        usedAnswers.push(correct.location);
+            
+        while(usedAnswers.length < numFalse){
+                
+            var potential = getRanLocation();
+                
+            if(notUsed(potential, usedAnswers)){
+                usedAnswers.push(potential);
+            }
+                
+            timeout++;
+                
+            if(timeout>timeoutMax){
+                usedAnswers.push(-1); 
+                break;
+            }
+        }
+        
+        var falseAnswers = usedAnswers.splice(1, usedAnswers.length);
+        
+        return falseAnswers;
+    }
+    else if(questionType === 3){ //Battle notable fact
+        usedAnswers.concact(correct.notables);
+            
+        while(usedAnswers.length < numFalse){
+                
+            var potential;
+            
+            if(timeout<timeoutFallback){
+                potential = getRanBatNotable();
+            }
+            else{ //Normal fact related to question couldn't be found in reasonable amount of searches. Using more general facts.
+                potential = getFallbackFact();
+            }
+            
+            if(notUsed(potential, usedAnswers)){
+                usedAnswers.push(potential);
+            }
+                
+            timeout++;
+                
+            if(timeout>timeoutMax){
+                usedAnswers.push(-1); 
+                break;
+            }
+        }
+        
+        var falseAnswers = usedAnswers.splice(correct.notables.length-1, usedAnswers.length); //Remove correct notable facts from array
+        
+        return falseAnswers;
+    }
+    else if(questionType === 4){ //Invention significance
+        usedAnswers.push(correct.significance);
+            
+        while(usedAnswers.length < numFalse){
+                        
+            var potential;
+            
+            if(timeout<timeoutFallback){
+                potential = getRanInvSig();
+            }
+            else{
+                potential = getFallbackFact();
+            }
+            
+            if(notUsed(potential, usedAnswers)){
+                usedAnswers.push(potential);
+            }
+                
+            timeout++;
+                
+            if(timeout>timeoutMax){
+                usedAnswers.push(-1); 
+                break;
+            }
+        }
+        
+        var falseAnswers = usedAnswers.splice(1, usedAnswers.length);
+        
+        return falseAnswers;
+    }
+    else if(questionType === 5){ //Election notable
+        usedAnswers.concact(correct.notables);
+            
+        while(usedAnswers.length < numFalse){
+                
+            var potential;
+            
+            if(timeout<timeoutFallback){
+                potential = getRanElecNotable();
+            }
+            else{
+                potential = getFallbackFact();
+            }
+                
+            if(notUsed(potential, usedAnswers)){
+                usedAnswers.push(potential);
+            }
+                
+            timeout++;
+                
+            if(timeout>timeoutMax){
+                usedAnswers.push(-1); 
+                break;
+            }
+        }
+        
+        var falseAnswers = usedAnswers.splice(correct.notables.length-1, usedAnswers.length);
+        
+        return falseAnswers;
+    }
+    else if(questionType === 6){ //Court ruling
+        usedAnswers.push(correct.ruling);
+            
+        while(usedAnswers.length < numFalse){
+                
+            var potential;
+            
+            if(timeout<timeoutFallback){
+                potential = getRanRuling();
+            }
+            else{
+                potential = getFallbackFact();
+            }
+                
+            if(notUsed(potential, usedAnswers)){
+                usedAnswers.push(potential);
+            }
+                
+            timeout++;
+                
+            if(timeout>timeoutMax){
+                usedAnswers.push(-1); 
+                break;
+            }
+        }
+        
+        var falseAnswers = usedAnswers.splice(1, usedAnswers.length);
+        
+        return falseAnswers;
+    }
+    else if(questionType === 7){ //Other significance
+        usedAnswers.push(correct.significance);
+            
+        while(usedAnswers.length < numFalse){
+                
+            var potential;
+            
+            if(timeout < timeoutFallback){
+                potential = getRanOtherSig();
+            }
+            else{
+                potential = getFallbackFact();
+            }
+                
+            if(notUsed(potential, usedAnswers)){
+                usedAnswers.push(potential);
+            }
+                
+            timeout++;
+                
+            if(timeout>timeoutMax){
+                usedAnswers.push(-1); 
+                break;
+            }
+        }
+        
+        var falseAnswers = usedAnswers.splice(1, usedAnswers.length);
+        
+        return falseAnswers;
+    }
+}
+
+
 function questionSetup(){
 
     $(".answerBox1").removeClass( "answerBox1Dropped" );
@@ -266,20 +477,54 @@ function questionSetup(){
     
     if(qSet <= qLimit){
         
-        //var event = getEvent();
         
         //Put name of event at top
         //Setup question boxes and correct answer bubbles
         
-        //var wrongAnswers = getFalseAnswers(event);
 
+//1 is year, 2 is location, 3 is battle notable, 4 is invention significance, 5 is election notable, 6 is court ruling, 7 is other significance
         var randoms = getRanEvent(); //randoms[0] is category number, randoms[1] is event number
         var cat= categories[randoms[0]];
-        var  event= randoms[1];
+        var  ranEvent= randoms[1];
         var questions = getQuestions(randoms[0]);
+        var falseAnswers1=[];
+        var falseAnswers2=[];
+
         $('#questionBox td').eq(0).html(questions[0]);
         $('#questionBox td').eq(2).html(questions[1]);
-        $("p.eventText").html(gameEvents[cat][event].name);
+        $("p.eventText").html(gameEvents[cat][ranEvent].name);
+        if(cat==="battles"){
+            $( "#a1" ).html(gameEvents[cat][ranEvent].year);
+            falseAnswers1=genFalseAnswers(1);
+            $( "#b1" ).html(gameEvents[cat][ranEvent].location);
+            falseAnswers2=genFalseAnswers(2);
+
+        }
+        else if(cat==="inventions"){
+            $( "#a1" ).html(gameEvents[cat][ranEvent].year);
+            falseAnswers1=genFalseAnswers(1);
+            $( "#b1" ).html(gameEvents[cat][ranEvent].significance);
+            falseAnswers2=genFalseAnswers(4);
+
+        }
+        else if(cat==="elections"){
+            $( "#a1" ).html(gameEvents[cat][ranEvent].result);
+            $( "#b1" ).html(gameEvents[cat][ranEvent].notables[0]); //TO BE RANDOMIZEd
+
+        }
+         else if(cat==="court"){
+            $( "#a1" ).html(gameEvents[cat][ranEvent].result);
+            $( "#b1" ).html(gameEvents[cat][ranEvent].ruling);
+
+        }
+
+         else if(cat==="other"){
+            $( "#a1" ).html(gameEvents[cat][ranEvent].year);
+            $( "#b1" ).html(gameEvents[cat][ranEvent].significance);
+
+        }
+
+
 
 
 
@@ -321,6 +566,91 @@ function questionSetup(){
     }
 }
 
+function notUsed(potential, usedAnswers){
+    for each(var used in usedAnswers){      
+        if(potential === used){
+            return false;
+        }
+    }
+    return true;
+}
+
+function getRanYear(){
+    var category = Math.floor(Math.random()*5)+1; //Randomly pick from categories
+    
+    if(category === 1){ //Battle
+        var index = Math.floor(Math.random()*gameEvents.battles.length)+1;
+        return gameEvents.battles[index].year;
+    }
+    else if(type === 2){ //Invention
+        var index = Math.floor(Math.random()*gameEvents.inventions.length)+1;
+        return gameEvents.inventions[index].year;
+    }
+    else if(type === 3){ //Election
+        var index = Math.floor(Math.random()*gameEvents.elections.length)+1;
+        return gameEvents.elections[index].year;
+    }
+    else if(type === 4){ //Court Case
+        var index = Math.floor(Math.random()*gameEvents.court.length)+1;
+        return gameEvents.court[index].year;
+    }
+    else if(type === 5){ //Other
+        var index = Math.floor(Math.random()*gameEvents.other.length)+1;
+        return gameEvents.other[index].year;
+    }
+}
+
+function getRanLocation(){
+    var index = Math.floor(Math.random()*gameEvents.battles.length)+1;
+    return gameEvents.battles[index].location;
+}
+
+function getRanBatNotable(){
+    var index = Math.floor(Math.random()*gameEvents.battles.length)+1;
+    var index2 = Math.floor(Math.random()*gameEvents.battles.notables.length);
+    return gameEvents.battles[index].notables[index2];
+}
+
+function getRanInvSig(){
+    var index = Math.floor(Math.random()*gameEvents.inventions.length)+1;
+    return gameEvents.inventions[index].significance;
+}
+
+function getRanElecNotable(){
+    var index = Math.floor(Math.random()*gameEvents.elections.length)+1;
+    var index2 = Math.floor(Math.random()*gameEvents.elections.notables.length);
+    return gameEvents.elections[index].notables[index2];
+}
+
+function getRanRuling(){
+    var index = Math.floor(Math.random()*gameEvents.court.length)+1;
+    return gameEvents.court[index].ruling;
+}
+
+function getRanOtherSig(){
+    var index = Math.floor(Math.random()*gameEvents.other.length)+1;
+    return gameEvents.other[index].significance;
+}
+
+function fallBackFacts(){
+    var category = Math.floor(Math.random()*5)+1; //Randomly pick from categories
+    
+    if(category === 1){ //Battle
+        return getRanBatNotable();
+    }
+    else if(type === 2){ //Invention
+        return getRanInvSig();
+    }
+    else if(type === 3){ //Election
+        return getRanElecNotable();
+    }
+    else if(type === 4){ //Court Case
+        return getRanRuling();
+    }
+    else if(type === 5){ //Other
+        return getRanOtherSig();
+    }
+}
 function getEvent(){
     //Get event from new JSON object of only selected event types
     var event;
